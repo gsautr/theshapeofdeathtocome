0677f378e3cb22f922c396b8fd36553a44356fbe
Gilbert <gilbertsinnott@Air.home>
2016-11-01 14:13:59 +0000

    Same controls

--- js/main.js
+++ js/main.js
@@ -261,6 +261,7 @@ function init() {
 
 
   controls = new THREE.FirstPersonControls( camera , renderer.domElement);
+  controls.movementSpeed = 0.1;
   controls.lookSpeed = 0.1;
   controls.activeLook = false;
   controls.autoForward =  false;
